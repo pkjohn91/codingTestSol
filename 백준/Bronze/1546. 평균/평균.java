c3b82e4b8706ef8179f7b42e833c1d4223750fa2
johnlog <78454742+pkjohn91@users.noreply.github.com>
2024-04-17 22:27:37 +0900

    [Bronze I] Title: 평균, Time: 256 ms, Memory: 18344 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int N = sc.nextInt();        
-        int[] arr = new int[N];
-        
-        for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
+        int N = sc.nextInt();
         
         long max = 0;
         long sum = 0;
         
         for(int i = 0; i < N; i++) {
-            //max찾기
-            if(arr[i] > max) max = arr[i];
-            sum = sum + arr[i];
+            int temp = sc.nextInt();
+            //max값 찾기
+            if(temp > max) max = temp;
+            sum = sum + temp;
         }
         
-        System.out.println(sum*100.0/max/N); //소숫점을 붙여주면 자동적으로 더블형으로 바꿔준다.
+        System.out.println(sum*100.0/max/N);
     }
 }
