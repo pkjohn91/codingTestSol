9adb878497b23b2fdab6c0907979c07842e0e940
johnlog <78454742+pkjohn91@users.noreply.github.com>
2024-05-13 10:52:13 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = my_string.substring(s, e+1);
+        StringBuffer sb = new StringBuffer(answer);
+        String reverse = sb.reverse().toString();
+        
+        my_string = my_string.replace(answer, reverse);
+        
+        
+        return my_string;
+    }
+}
