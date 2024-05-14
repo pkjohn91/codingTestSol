9f5c0caa67e190042ca450cad5c6ccad967ec6ee
johnlog <78454742+pkjohn91@users.noreply.github.com>
2024-05-14 15:14:10 +0900

    [level 0] Title: qr code, Time: 0.16 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,13 @@
+import java.lang.StringBuffer;
+
+class Solution {
+    public String solution(int q, int r, String code) {
+
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < code.length(); i++) {
+            if(i%q == r) sb.append(code.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
