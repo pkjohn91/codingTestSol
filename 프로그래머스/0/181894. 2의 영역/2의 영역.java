0cf984c1e953a32c6027fdf0174338a8fa14ac24
johnlog <78454742+pkjohn91@users.noreply.github.com>
2024-05-17 22:34:36 +0900

    [level 0] Title: 2의 영역, Time: 1.31 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int max = 0;
+        int min = 100000;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 2) {
+                min = Math.min(min, i);
+                max = Math.max(max, i);
+            }
+        }
+        
+        if(max >= min) {
+            return Arrays.copyOfRange(arr, min, max+1);
+        } else {
+            return new int[]{-1};
+        }
+        
+    }
+}
