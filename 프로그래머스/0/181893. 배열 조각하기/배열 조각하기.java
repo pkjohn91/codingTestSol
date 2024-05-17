8a24272a7f6c6871bed2d452e0dd7e40c286a729
johnlog <78454742+pkjohn91@users.noreply.github.com>
2024-05-17 23:26:27 +0900

    [level 0] Title: 배열 조각하기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
+        int start = 0;
+        int end = arr.length - 1;
+        
         for(int i = 0; i < query.length; i++) {
-            if(i % 2 == 0) { // 짝수
-                arr = Arrays.copyOfRange(arr, 0, query[i]+1);
-            }
-            else { // 홀수
-                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            if(i % 2 == 0) {
+                end = start + query[i] - 1;
+            } else {
+                start += query[i];
             }
         }
-        return arr;
+        return Arrays.copyOfRange(arr, start, end+2);
     }
 }
