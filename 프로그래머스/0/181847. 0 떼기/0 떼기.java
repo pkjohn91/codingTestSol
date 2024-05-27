3f26f17ff2f5c9b3c621fecad982598ec5f61db9
Park. Jonghun <78454742+pkjohn91@users.noreply.github.com>
2024-05-27 11:19:34 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int n = Integer.parseInt(n_str);
+        answer = String.valueOf(n);
+        return answer;
+    }
+}
