819fb5a024819f38aafed4884c66b6c8f7767045
Park. Jonghun <78454742+pkjohn91@users.noreply.github.com>
2024-07-08 14:13:47 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numer = (numer1*denom2) + (numer2*denom1);
+        int denom = denom1 * denom2;
+        int max = -1;
+        // 최대공약수 찾기
+        for(int i = 1; i <= denom; i++) {
+            if(numer % i == 0 && denom % i == 0) {
+                max = i;
+            }
+        }
+        numer = numer/max;
+        denom = denom/max;
+        int[] answer = {numer, denom};
+        return answer;
+    }
+}
