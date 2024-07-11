700a17d2a09edd81ebf2d847f45b425e90188e6a
Park. Jonghun <78454742+pkjohn91@users.noreply.github.com>
2024-07-11 17:35:05 +0900

    [Silver III] Title: 소수 구하기, Time: 744 ms, Memory: 35812 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,12 +1,15 @@
 import java.util.*;
+import java.io.*;
 
 class Main {
-	public static void main (String[] args) {
-	    Scanner in = new Scanner(System.in);
-		int m = in.nextInt();
-		int n = in.nextInt();
+	public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 		int[] a = new int[n+1];
 
+        br.close();
         // 배열 초기화
         for(int i = 2; i <= n; i++) {
             a[i] = i;
