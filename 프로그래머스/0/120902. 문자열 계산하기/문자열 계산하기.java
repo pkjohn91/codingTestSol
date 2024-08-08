104bc0b47132dc6ca62789b96d15985c5f0a21ad
Park. Jonghun <78454742+pkjohn91@users.noreply.github.com>
2024-08-09 01:27:04 +0900

    [level 0] Title: 문자열 계산하기, Time: 6.84 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -2,13 +2,24 @@ class Solution {
     public int solution(String my_string) {
         String[] s = my_string.split(" ");
         int answer = Integer.parseInt(s[0]);
+        int idx = 2;
         for (int i = 1; i < s.length-1; i+=2) {
             if(s[i].equals("+")) {
-                answer += Integer.parseInt(s[i+1]);
-            } else if (s[i].equals("-")) {
-                answer -= Integer.parseInt(s[i+1]);
+                answer += Integer.parseInt(s[idx]);
+                idx += 2;
+                System.out.println("answer = " + answer);
+                System.out.println("Integer index = " + idx);
+                System.out.println("=================");
+            } else {
+                answer -= Integer.parseInt(s[idx]);
+                idx += 2;
+                System.out.println("answer = " + answer);
+                System.out.println("Integer index = " + idx);
+                System.out.println("=================");
             }
         }
+        
+        
         return answer;
     }
 }
