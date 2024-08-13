8be658649ae289e39256874bb5c54d3cda14731a
Park. Jonghun <78454742+pkjohn91@users.noreply.github.com>
2024-08-13 20:27:27 +0900

    [level 0] Title: 숫자 찾기, Time: 8.61 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        String[] s = String.valueOf(num).split("");
-        int answer = 0;
         
-        for (int i = 0; i < s.length; i++) {
-            if(Integer.parseInt(s[i]) == k) {
-                return i+1;
-            }
-        }
-        return -1;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
