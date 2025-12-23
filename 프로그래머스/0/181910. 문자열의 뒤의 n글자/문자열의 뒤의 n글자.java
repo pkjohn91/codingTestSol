c0bb2586d907740c8cb78fa583be7879b63f0009
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2025-12-23 14:52:16 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = my_string.substring(my_string.length()-n);
-        
-        return answer;
+        String answer = "";
+        return my_string.substring(my_string.length()-n);
     }
 }
