21bc5923c5f69b72ca5efae5bcf1960dfb3e864b
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2025-12-30 15:25:39 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        boolean answer = my_string.startsWith(is_prefix);
-        return answer ? 1 : 0;
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
