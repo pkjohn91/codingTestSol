24cc071178d1bdba1c637dac283d87083a1bd4d2
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2026-01-09 23:54:28 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 6.17 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int k) {
-        for(int i = 0; i < arr.length; i++) {
-            if(k % 2 == 0) {
-                arr[i] += k;
+        if (k % 2 != 0) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = arr[i] * k;
             }
-            else {
-                arr[i] *= k;
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = arr[i] + k;
             }
         }
         return arr;
