4e41aa1a1d9f71e01ce7cbb0decbfa42591a83ec
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2026-01-09 23:45:50 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 4.33 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,6 +1,17 @@
 class Solution {
     public String solution(String my_string, String alp) {
-        String answer = my_string.replaceAll(alp, alp.toUpperCase());
+        String answer = "";
+        String[] arr = my_string.split("");
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(alp)) {
+                arr[i] = alp.toUpperCase();
+            }
+        }
+        
+        for (String s : arr) {
+            answer += s;
+        }
         return answer;
     }
 }
