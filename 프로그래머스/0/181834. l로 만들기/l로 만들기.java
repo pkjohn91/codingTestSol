052b1d36a4b06c658aaa600e8c8baaa42d74261e
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2026-01-10 00:15:05 +0900

    [level 0] Title: l로 만들기, Time: 1178.77 ms, Memory: 385 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,5 +1,20 @@
 class Solution {
     public String solution(String myString) {
-        return myString.replaceAll("[a-k]", "l");
+        char[] arr = myString.toCharArray();
+        int lAsciiNum = 108;
+        
+        for (int i = 0; i < arr.length; i++) {
+            int arrAsciiNum = (int) arr[i];
+            if (lAsciiNum > arrAsciiNum) {
+                arr[i] = (char) lAsciiNum;
+            }
+        }
+        
+        String answer = "";
+        for (char c : arr) {
+            answer += String.valueOf(c);
+        }
+        
+        return answer;
     }
 }
