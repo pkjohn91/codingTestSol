cf13df625cc2ca66e87a9afbfdde04ebe72a6315
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2026-01-10 13:41:24 +0900

    [Bronze IV] Title: 숫자의 합, Time: 168 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt(); // nextInt()는 숫자만 받고 \n를 버리고 return
-        
-        String str = sc.next(); // nextLine()를 쓰면 그 전에 숫자 입력값+\n(개행)을 받았기 때문에 \n이 포함되서 바로 종료됌
+        sc.nextInt();
+        String str = sc.next();
         char[] arr = str.toCharArray();
         
         int sum = 0;
-        for(int i = 0; i < arr.length; i++) {
-            sum += arr[i] - '0';
+        for (char c : arr) {
+            sum += Integer.parseInt(String.valueOf(c));
         }
         
-        System.out.println(sum);
+        System.out.printf("%d", sum);
     }
 }
