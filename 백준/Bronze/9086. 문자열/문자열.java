014f31ee468e67fb97fa012b5637bc961ca62691
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2026-01-10 15:25:05 +0900

    [Bronze V] Title: 문자열, Time: 184 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for(int i = 0; i < N; i++) {
+            String s = sc.next();
+            System.out.printf("%s%s\n", s.charAt(0), s.charAt(s.length()-1));
+        }
+    }
+}
