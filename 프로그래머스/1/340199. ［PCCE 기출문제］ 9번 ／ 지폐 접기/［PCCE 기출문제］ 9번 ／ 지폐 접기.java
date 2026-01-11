81a3c65e1b45e1b54e38ff9aaaba2e948f4ded00
박 종 훈 (Park. Jonghun) <78454742+pkjohn91@users.noreply.github.com>
2026-01-11 12:40:52 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.05 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        
+        while (true) {
+            int w_min = Math.min(wallet[0], wallet[1]);
+            int b_min = Math.min(bill[0], bill[1]);
+            int w_max = Math.max(wallet[0], wallet[1]);
+            int b_max = Math.max(bill[0], bill[1]);
+            
+            if (b_min > w_min || b_max > w_max) {
+                if (bill[0] > bill[1]) {
+                    bill[0] /= 2;
+                } else {
+                    bill[1] /= 2;
+                }
+                answer++;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
